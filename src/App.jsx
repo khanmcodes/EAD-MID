23025fd4f5b6452958a7bb7abbc206fdc7204d94
khanmcodes <kkhalepoto/@gmail.com>
2025-03-24 15:49:52 -0700

    Practicing

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-green-900 flex justify-self-center justify-center w-100 border-2 border-green-400 rounded-md my-4 p-2">
+    <h1 className='text-center text-white font-mono font-extrabold'>Hello World</h1>
+    </div>
+
+    <div className="flex justify-center space-x-5 mx-5">
+      <div className="h-16 w-16 rounded-full bg-amber-400"></div>
+      <div className="h-16 w-16 rounded-full bg-amber-600"></div>
+      <div className="h-16 w-16 rounded-full bg-amber-800"></div>
+    </div>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcssc from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
